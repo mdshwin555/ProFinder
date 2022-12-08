fbde69ed2fc78fc5ecc9397d9b628273df5cc2b3
mdshwin555 <shmouayad305@gmail.com>
2022-12-08 15:55:40 +0300

    add settings screen

--- lib/view/screens/user/Settings.dart
+++ lib/view/screens/user/Settings.dart
@@ -1,14 +1,1 @@
-import 'package:flutter/material.dart';
 
-class Settings extends StatelessWidget {
-  const Settings({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-    );
-  }
-}
